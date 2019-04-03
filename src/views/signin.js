8b8543d3c14ef5f4ad41ce7606cd94eece40b9c8
Muhammad Ehsan <syedehsans1@gmail.com>
2019-04-03 23:12:15 +0500

    onChangeText method added to text fields

--- src/views/signin.js
+++ src/views/signin.js
@@ -1,59 +1,85 @@
 import React, { Component } from "react";
 import { View, Text, Image } from "react-native";
-import { Container, Header, Content, Form, Item, Input, Button } from "native-base";
+import {
+  Container,
+  Header,
+  Content,
+  Form,
+  Item,
+  Input,
+  Button
+} from "native-base";
 import { styles } from "../styles/signin.styles";
 
 class Signin extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      email: "",
+      password: ""
+    };
   }
 
   render() {
     return (
       <React.Fragment>
-        <Container style = {styles.header}>
-        <Image
+        <Container style={styles.header}>
+          <Image
             style={{ width: 300, height: 175 }}
             style={styles.logo}
             source={require("../../assets/signUp.png")}
           />
         </Container>
 
-        <Container style = {styles.body}>
-        <Container style={styles.container}>
-          <Content style={styles.content}>
-            <Item style={styles.inputFields}>
-              <Input placeholder="Username" />
-            </Item>
-            <Item style={styles.inputFields}>
-              <Input placeholder="Password" />
-            </Item>
-          
-            <View style={styles.buttonView}>
-              <Button style={[styles.button]} onPress={()=>this.props.navigation.navigate('Dashboard')}>
-                <Text style={{ color: "white" }}>Sign In</Text>
-              </Button>
-            </View>
+        <Container style={styles.body}>
+          <Container style={styles.container}>
+            <Content style={styles.content}>
+              <Item style={styles.inputFields}>
+                <Input
+                  placeholder="Email"
+                  onChangeText={email => this.setState({ email })}
+                />
+              </Item>
+              <Item style={styles.inputFields}>
+                <Input
+                  secureTextEntry={true}
+                  placeholder="Password"
+                  onChangeText={password => this.setState({ password })}
+                />
+              </Item>
 
-            <View style={styles.inputFields}
-                  style={styles.forgotPasswordView}>
-              <Text style={styles.forgotPasswordText}>Forgot Password ?</Text>
-            </View>
+              <View style={styles.buttonView}>
+                <Button
+                  style={[styles.button]}
+                  onPress={() => this.props.navigation.navigate("Dashboard")}
+                >
+                  <Text style={{ color: "white" }}>Sign In</Text>
+                </Button>
+              </View>
 
-          </Content>
+              <View
+                style={styles.inputFields}
+                style={styles.forgotPasswordView}
+              >
+                <Text style={styles.forgotPasswordText}>Forgot Password ?</Text>
+              </View>
+            </Content>
+          </Container>
         </Container>
-        </Container>
 
-        <Container style = {styles.footer}>
-
-        <View style={styles.buttonView}>
-              <Button style={styles.signupButton} onPress={()=>this.props.navigation.navigate('Signup')}>
-                <Text style={{ color: "grey",fontSize: 18 }}>Create Account</Text>
-              </Button>
-            </View>
+        <Container style={styles.footer}>
+          <View style={styles.buttonView}>
+            <Button
+              style={styles.signupButton}
+              onPress={() => this.props.navigation.navigate("Signup")}
+            >
+              <Text style={{ color: "grey", fontSize: 18 }}>
+                Create Account
+              </Text>
+            </Button>
+          </View>
         </Container>
-      </React.Fragment>  
+      </React.Fragment>
     );
   }
 }
