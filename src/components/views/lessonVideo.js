2d9e1cfda50d5ffd709f3ce375fea57e5f4d2193
Usman Shabbir <usmanshabbir94@gmail.com>
2019-04-14 14:17:40 +0500

    adding video weblink

--- src/components/views/lessonVideo.js
+++ src/components/views/lessonVideo.js
@@ -1,11 +1,8 @@
 import React , { Component } from 'react';
-import { Video } from 'expo';
-import VideoPlayer from '@expo/videoplayer';
-
+import { WebView } from 'react-native';
 export default class LessonVideo extends Component {
   constructor(props){
     super(props);
-    console.log("this.propse",this.props.navigation.state.params.source);
     this.state = {}
   }
 
@@ -22,30 +19,11 @@ export default class LessonVideo extends Component {
       }
     } = this;
 
-    console.log("this.props.source",link);
-
     return (
-      // <Video
-      //   source={{ uri: 'https://www.youtube.com/embed/r7CWEE3ZAWs' }}
-      //   rate={1.0}
-      //   volume={1.0}
-      //   isMuted={false}
-      //   resizeMode="cover"
-      //   shouldPlay
-      //   isLooping
-      //   style={{ width: 300, height: 300 }}
-      // />
-      <VideoPlayer
-        videoProps={{
-          shouldPlay: true,
-          resizeMode: Video.RESIZE_MODE_CONTAIN,
-          source: {
-            uri: 'https://www.youtube.com/watch?v=dyi-Qh_HeTI',
-          },
-        }}
-        isPortrait={true}
-        playFromPositionMillis={0}
-        playThroughEarpieceAndroid={true}
+      <WebView 
+        style={{flex: 1 }} 
+        javaScriptEnabled={true}
+        source={{uri: link}}
       />
     )
   }
